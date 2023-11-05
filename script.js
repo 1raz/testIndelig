9d3d3937fe6b84a6dbc56c668add02e85ac0b148
1raz <30551375+1raz@users.noreply.github.com>
2023-11-05 20:56:57 +0300

    test

--- script.js
+++ script.js
@@ -1,68 +1,37 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+// script.js
 
-#pragma pack(push, 1)
+// Получаем ссылку на контейнер для контента
+const contentContainer = document.getElementById('content');
 
-// Структура заголовка BMP файла
-struct BMPHeader {
-    uint16_t signature;     // Сигнатура "BM"
-    uint32_t fileSize;     // Размер файла
-    uint32_t reserved;     // Зарезервировано (0)
-    uint32_t dataOffset;   // Смещение до данных
-    uint32_t headerSize;   // Размер заголовка изображения
-    int32_t width;         // Ширина изображения
-    int32_t height;        // Высота изображения
-    uint16_t planes;       // Количество плоскостей (1)
-    uint16_t bitDepth;     // Глубина цвета (1 бит)
-    uint32_t compression;  // Сжатие (0 - без сжатия)
-    uint32_t imageSize;    // Размер изображения
-    int32_t ppmX;          // Горизонтальное разрешение (пикселей в метре)
-    int32_t ppmY;          // Вертикальное разрешение (пикселей в метре)
-    uint32_t colorUsed;    // Используемые цвета (0 - все)
-    uint32_t colorImportant;  // Важные цвета (0 - все)
-};
+// Обработчики событий для навигации между страницами
+document.querySelector('a[href="index.html"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    loadPage('index.html');
+});
 
-#pragma pack(pop)
+document.querySelector('a[href="products.html"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    loadPage('products.html');
+});
 
-int main() {
-    std::ifstream is("input.bmp", std::ios::binary);
+document.querySelector('a[href="about.html"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    loadPage('about.html');
+});
 
-    if (!is.is_open()) {
-        std::cerr << "Unable to open the input file." << std::endl;
-        return 1;
-    }
+document.querySelector('a[href="contacts.html"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    loadPage('contacts.html');
+});
 
-    BMPHeader bmpHeader;
-
-    is.read(reinterpret_cast<char*>(&bmpHeader), sizeof(BMPHeader));
-
-    if (bmpHeader.signature != 0x4D42) {
-        std::cerr << "Not a valid BMP file." << std::endl;
-        return 1;
-    }
-
-    // Обновление горизонтального и вертикального разрешения
-    bmpHeader.ppmX = 3000; // Новое горизонтальное разрешение (пикселей в метре)
-    bmpHeader.ppmY = 3000; // Новое вертикальное разрешение (пикселей в метре)
-
-    // Открываем файл для записи
-    std::ofstream os("output.bmp", std::ios::binary);
-
-    // Записываем обновленный заголовок BMP
-    os.write(reinterpret_cast<char*>(&bmpHeader), sizeof(BMPHeader));
-
-    // Копируем оставшуюся часть файла (данные изображения)
-    char buffer[1024];
-    while (!is.eof()) {
-        is.read(buffer, sizeof(buffer));
-        os.write(buffer, is.gcount());
-    }
-
-    is.close();
-    os.close();
-
-    std::cout << "Updated BMP file saved as 'output.bmp'" << std::endl;
-
-    return 0;
+// Функция для загрузки страницы
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            contentContainer.innerHTML = data;
+        });
 }
+
+// Загрузка главной страницы при старте
+loadPage('index.html');
